303fd4db0197f133aaf2bffca94b461b286fa21c
mariidaz <mariapar2005@gmail.com>
2025-11-30 19:39:40 +0200

    Final fix: myapp files added

---
+++ myapp/models/Product.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [2, "Name must be at least 2 characters long"]
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: 0,
+    },
+    description: {
+      type: String,
+      default: "",
+      maxlength: [200, "Description too long"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { versionKey: false }
+);
+
+export const Product = mongoose.model("Product", productSchema);
